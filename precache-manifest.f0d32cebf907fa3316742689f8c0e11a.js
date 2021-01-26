b59a8194c79561b3e9d234b8312f1bb461c35d8f
Dav3rs <davidiazfz@gmail.com>
2021-01-25 22:46:40 -0500

    deploy

---
+++ precache-manifest.f0d32cebf907fa3316742689f8c0e11a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85316792a179fe7df1b8",
+    "url": "/hello-world/css/app.b11c3cc3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/hello-world/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8972742821727258a82600fb316edc80",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/hello-world/js/about.063687bd.js"
+  },
+  {
+    "revision": "85316792a179fe7df1b8",
+    "url": "/hello-world/js/app.f6b13e98.js"
+  },
+  {
+    "revision": "d2007447d54967bc52fe",
+    "url": "/hello-world/js/chunk-vendors.5de6a343.js"
+  },
+  {
+    "revision": "f9de52036eb0a537fb8413498acda663",
+    "url": "/hello-world/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello-world/robots.txt"
+  }
+]);
